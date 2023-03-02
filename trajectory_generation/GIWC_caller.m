c6d22654826762209af5e6cfe14c6c5464e84610
STaliani <104269855+STaliani@users.noreply.github.com>
2023-03-02 23:05:54 +0100

    Add files via upload

---
+++ trajectory_generation/GIWC_caller.m
@@ -0,0 +1,11 @@
+clc 
+clear all
+close all
+surfaces= get_surf1('four_points_nonflat');
+CoM=[0;0;10];
+% figure 
+% answ1=gravito_inertial_wrench_corretto(surfaces(1:2),CoM);
+% axis equal
+figure
+answ2=gravito_inertial_wrench_prova_m(surfaces(1:2),CoM);
+axis equal
